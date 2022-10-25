b91fc9b629f35f179932d9d6b125bcee86aa1470
Sreerag M <sreeragibtl@gmail.com>
2022-10-25 15:34:30 +0530

    dev: Add train_custom

---
+++ train_custom.py
@@ -0,0 +1,71 @@
+"""Train the model in distributed fashion
+
+Train the model in distributed fashion
+
+Usage:
+    python -m train_custom
+"""
+
+import argparse
+import os
+import typing
+
+import torch
+
+def train(
+    rank: int, 
+    arguments: typing.Tuple
+    ) -> None:
+
+    """Train the model
+
+    Train the model in multiple GPUs.
+
+    Args:
+        rank: The id of the process.
+        arguments: Arguments to the funtion.
+
+    Returns:
+        None
+    """
+
+    arguments.initialize_distribution(
+        backend=arguments.backend,
+        rank=arguments.rank,
+        world_size=arguments.world_size,
+    )
+
+if __name__ == "__main__":
+
+    parser: argparse.ArgumentParser = argparse.ArgumentParser(__doc__)
+
+    parser.add_argument(
+        "--dataset", 
+        type=str,
+        help="Root of the dataset",
+        required=True,
+    )
+
+    parser.add_argument(
+        "--checkpoint",
+        type=str,
+        help="Location to store checkpoint",
+        required=True,
+    )
+
+    parser.add_argument(
+        "--hosts",
+        type=list,
+        help="List of hosts to use",
+        required=True,
+    )
+
+    arguments = parser.parse_args()
+
+    os.environ["WORLD_SIZE"] = str(arguments.hosts)
+
+    torch.multiprocessing.spawn(
+        fn=train,
+        args=(arguments,),
+        nprocs=len(arguments.hosts),
+        )
