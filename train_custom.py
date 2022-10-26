79c18f33d92373ec2982e48f010f035f121cb21e
Sreerag M <sreeragibtl@gmail.com>
2022-10-26 15:32:40 +0530

    dev: Set port and address

--- train_custom.py
+++ train_custom.py
@@ -78,6 +78,9 @@ if __name__ == "__main__":
 
     arguments = parser.parse_args()
 
+    os.environ["MASTER_ADDR"] = "localhost"
+    os.environ["MASTER_PORT"] = "12355"
+
     os.environ["WORLD_SIZE"] = str(len(arguments.hosts))
     os.environ["RANK"] = str(arguments.hosts.index(arguments.current_host))
 
